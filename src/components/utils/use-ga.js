9a669a0379555ddc0464970627929a1131637cd8
Punith K <kpunith8@gmail.com>
2020-06-27 21:26:21 +0530

    Fixed env variable setting issue

--- src/components/utils/use-ga.js
+++ src/components/utils/use-ga.js
@@ -1,5 +1,4 @@
 import { useEffect } from "react";
-import { GOOGLE_ANALYTICS_ID } from "./site-env";
 import ReactGA from "react-ga";
 
 const GOOGLE_ANALYTICS_ID = process.env.REACT_APP_GOOGLE_ANALYTICS_ID;
